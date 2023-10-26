b2a26f9089bd6a4da3eb70375e78a5a2b69d2411
Cherry <rene9917@gmail.com>
2023-10-26 07:35:41 +0200

    Split message if it's too long

--- openai.go
+++ openai.go
@@ -21,8 +21,9 @@ func sendMessageResponse(s *discordgo.Session, m *discordgo.MessageCreate) {
 	c := openai.NewClient(openaiToken)
 	ctx := context.Background()
 
+	// Create a new request
 	req := openai.ChatCompletionRequest{
-		Model: openai.GPT40613,
+		Model: openai.GPT40314,
 		Messages: []openai.ChatCompletionMessage{
 			{
 				Role:    openai.ChatMessageRoleUser,
@@ -31,19 +32,24 @@ func sendMessageResponse(s *discordgo.Session, m *discordgo.MessageCreate) {
 		},
 		Stream: true,
 	}
+	// Send the request
 	stream, err := c.CreateChatCompletionStream(ctx, req)
 	if err != nil {
 		log.Printf("ChatCompletionStream error: %v\n", err)
 		return
 	}
 	defer stream.Close()
-	msg := sendMessage(s, m, "Responding...")
-	var i int = 1
 
+	msg := sendMessage(s, m, "Responding...")
+
+	var i int
 	var message string
+	// Read the stream and send the response
 	for {
 		response, err := stream.Recv()
 		if errors.Is(err, io.EOF) {
+			// At the end of the stream
+			// Send the last message delta
 			editMessage(s, msg, message)
 			return
 		}
@@ -53,10 +59,18 @@ func sendMessageResponse(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 
 		message = message + response.Choices[0].Delta.Content
-		if i%15 == 0 {
-			editMessage(s, msg, message)
-		}
 		i++
+		// Every 15 delta send the message
+		if i%15 == 0 {
+			// If the message is too long, split it into a new message
+			if len(message) > 1900 {
+				editMessage(s, msg, message)
+				msg = sendMessage(s, m, "...")
+				message = ""
+			} else {
+				editMessage(s, msg, message)
+			}
+		}
 	}
 }
 
@@ -71,7 +85,7 @@ func sendInteractionResponse(s *discordgo.Session, i *discordgo.InteractionCreat
 	ctx := context.Background()
 
 	req := openai.ChatCompletionRequest{
-		Model: openai.GPT40613,
+		Model: openai.GPT40314,
 		Messages: []openai.ChatCompletionMessage{
 			{
 				Role:    openai.ChatMessageRoleUser,
@@ -103,7 +117,13 @@ func sendInteractionResponse(s *discordgo.Session, i *discordgo.InteractionCreat
 
 		message = message + response.Choices[0].Delta.Content
 		if count%15 == 0 {
-			editFollowup(s, i, msg.ID, message)
+			if len(message) > 1900 {
+				editFollowup(s, i, msg.ID, message)
+				msg = sendFollowup(s, i, "...")
+				message = ""
+			} else {
+				editFollowup(s, i, msg.ID, message)
+			}
 		}
 		count++
 	}
